2cb0b60ffc4f64fff5a18daf436bbef982e50adc
amalikrigger <amalikrigger93@gmail.com>
2023-09-20 10:38:32 -0400

    removing load extension

--- CandySpace/ViewController/GalleryViewController.swift
+++ CandySpace/ViewController/GalleryViewController.swift
@@ -125,17 +125,3 @@ extension GalleryViewController: UICollectionViewDelegateFlowLayout {
         return 5
       }
 }
-
-extension UIImageView {
-    func load(url: URL) {
-        DispatchQueue.global().async { [weak self] in
-            if let data = try? Data(contentsOf: url) {
-                if let image = UIImage(data: data) {
-                    DispatchQueue.main.async {
-                        self?.image = image
-                    }
-                }
-            }
-        }
-    }
-}

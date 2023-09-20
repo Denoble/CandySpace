05eb50abf097bc6793a734d1d23e4bf177657812
amalikrigger <amalikrigger93@gmail.com>
2023-09-20 00:16:19 -0400

    cacheimplementation

--- CandySpace/Cache.swift
+++ CandySpace/Cache.swift
@@ -9,13 +9,14 @@ import Foundation
 import UIKit
 
 class ImageCache {
+    static let shared = ImageCache()
     private let cache = NSCache<NSString, UIImage>()
 
     func getImage(url: String) -> UIImage? {
         return cache.object(forKey: url as NSString)
     }
 
-    func setImage(image: UIImage?, for url: String) {
+    func setImage(image: UIImage?, url: String) {
         if let image = image {
             cache.setObject(image, forKey: url as NSString)
         }
@@ -23,13 +24,14 @@ class ImageCache {
 }
 
 class SearchResultCache {
-    private var cache = [String: [SearchResult]]()
+    static let shared = SearchResultCache()
+    private var cache = [String: [Hits]]()
 
-    func getResults(searchTerm: String) -> [SearchResult]? {
+    func getResults(searchTerm: String) -> [Hits]? {
         return cache[searchTerm]
     }
 
-    func setResults(results: [SearchResult], searchTerm: String) {
+    func setResults(results: [Hits], searchTerm: String) {
         cache[searchTerm] = results
     }
 }
@@ -37,41 +39,3 @@ class SearchResultCache {
 struct SearchResult {
     let imageUrl: String
 }
-
-//func performSearch(for searchTerm: String) {
-//    // Check if results are already cached
-//    if let cachedResults = SearchResultCache.shared.getResults(for: searchTerm) {
-//        // Use cached results
-//        displayResults(cachedResults)
-//    } else {
-//        // Perform the search (e.g., fetch data from a server)
-//        fetchData(for: searchTerm) { results in
-//            // Cache the results
-//            SearchResultCache.shared.setResults(results, for: searchTerm)
-//
-//            // Display the results
-//            displayResults(results)
-//        }
-//    }
-//}
-//
-//if let imageUrl = URL(string: "https://example.com/image.jpg") {
-//    if let cachedImage = ImageCache.shared.getImage(for: imageUrl.absoluteString) {
-//        // Use the cached image
-//        imageView.image = cachedImage
-//    } else {
-//        // Download the image
-//        URLSession.shared.dataTask(with: imageUrl) { data, _, _ in
-//            if let data = data, let image = UIImage(data: data) {
-//                // Cache the downloaded image
-//                ImageCache.shared.setImage(image, for: imageUrl.absoluteString)
-//
-//                // Set the image in UIImageView (on the main queue)
-//                DispatchQueue.main.async {
-//                    imageView.image = image
-//                }
-//            }
-//        }.resume()
-//    }
-//}
-
--- CandySpace/GalleryViewModel.swift
+++ CandySpace/GalleryViewModel.swift
@@ -6,23 +6,60 @@
 //
 
 import Foundation
-class GalleryViewModel{
-    let networkManager:NetworkManagerHelper
-    init(initNetworkManager:NetworkManagerHelper){
-        networkManager = initNetworkManager
-    }
-  lazy  var hitsArray = HitArrays()
+import UIKit
 
-    func getImageGallery(searchParameter:String) async {
-        
-        guard let url = CandySpaceURL.getGalleryurl(q: searchParameter) else{
-            return }
-        do{
-            let gallery = try await networkManager.taskForGETRequest(url: url, responseType: Gallery.self)
-            hitsArray = gallery.hits ?? HitArrays()
-        }catch{
-            print(String(describing: error))
+class GalleryViewModel {
+    let networkManager: NetworkManagerHelper
+    lazy var images = HitArrays()
+    init(networkManager: NetworkManagerHelper) {
+        self.networkManager = networkManager
+    }
+
+    func getImageGallery(searchTerm: String) async {
+        if let cachedImages = getCachedImages(searchTerm: searchTerm) {
+            images = cachedImages
+            return
         }
-        
+        guard let url = CandySpaceURL.getGalleryurl(searchTerm: searchTerm) else {
+            return
+        }
+        do {
+            let result = try await networkManager.get(url: url, responseType: Gallery.self)
+            switch result {
+            case .success(let res):
+                images = res.hits ?? []
+                setCachedImages(hits: images, searchTerm: searchTerm)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    func getCachedImages(searchTerm: String) -> HitArrays? {
+        if let cachedResults = SearchResultCache.shared.getResults(searchTerm: searchTerm) {
+            return cachedResults
+        }
+        return nil
+    }
+    func setCachedImages(hits: HitArrays, searchTerm: String) {
+        SearchResultCache.shared.setResults(results: hits, searchTerm: searchTerm)
+    }
+    func getImage(url: String) -> UIImage? {
+        if let imageUrl = URL(string: "https://example.com/image.jpg") {
+            if let cachedImage = ImageCache.shared.getImage(url: imageUrl.absoluteString) {
+                return cachedImage
+            } else {
+                var img = UIImage()
+                URLSession.shared.dataTask(with: imageUrl) { data, _, _ in
+                    if let data = data, let image = UIImage(data: data) {
+                        ImageCache.shared.setImage(image: image, url: imageUrl.absoluteString)
+                        img = image
+                    }
+                }.resume()
+                return img
+            }
+        }
+        return nil
     }
 }

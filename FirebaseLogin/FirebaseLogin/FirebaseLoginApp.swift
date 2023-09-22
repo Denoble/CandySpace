31488943b707ba6cdf71f6a7312036dc482d720a
Lawrence <consultant@MBP-C02ZT45VMD6M-TESCHROEDER.local>
2023-09-22 12:24:03 -0400

    project init

---
+++ FirebaseLogin/FirebaseLogin/FirebaseLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLoginApp.swift
+//  FirebaseLogin
+//
+//  Created by Lawrence on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

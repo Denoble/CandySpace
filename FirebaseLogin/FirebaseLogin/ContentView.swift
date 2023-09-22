b84a270290f83856b873dcccab5bc607bbfe7fdc
Lawrence <consultant@MBP-C02ZT45VMD6M-TESCHROEDER.local>
2023-09-22 14:10:40 -0400

    firebase implemented

--- FirebaseLogin/FirebaseLogin/ContentView.swift
+++ FirebaseLogin/FirebaseLogin/ContentView.swift
@@ -6,16 +6,33 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Email", text: $email)
+            SecureField("Password", text: $password)
+            Button(action: { login() }) {
+                Text("Sign in")
+            }
         }
         .padding()
+    }
+    
+    func login() {
+        print("Email: \(email)")
+        print("Password: \(password)")
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+            } else {
+                print("success")
+            }
+        }
     }
 }
 

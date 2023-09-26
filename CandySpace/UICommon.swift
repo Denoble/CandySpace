cd3a07518b3b1b89653133c3f87c8e66df1001ed
Will <william.rozier@techconsulting.tech>
2023-09-26 17:48:08 -0400

    Login View changes

---
+++ CandySpace/UICommon.swift
@@ -0,0 +1,21 @@
+//
+//  UICommon.swift
+//  CandySpace
+//
+//  Created by William Rozier on 9/26/23.
+//
+
+import SwiftUI
+
+
+extension CGFloat {
+    
+    static var screenWidth: Double {
+        return UIScreen.main.bounds.size.width
+    }
+    
+    static var screenHeight: Double {
+        return UIScreen.main.bounds.size.height
+    }
+    
+}
--- CandySpace/ViewController/LoginView.swift
+++ CandySpace/ViewController/LoginView.swift
@@ -12,34 +12,52 @@ struct LoginView: View {
     @State var userEmail: String = ""
     @State var userPassword: String = ""
     var body: some View {
-        VStack {
-            HStack {
-                Text("Email: ")
-                TextField("Enter Email", text: $userEmail)
-                    .keyboardType(.emailAddress)
+        ZStack {
+            Image("splash_bg")
+             .resizable()
+             .scaledToFill()
+             .frame( width: .screenWidth,
+                     height: .screenHeight )
+            VStack {
+                Image("app_logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:100)
+                    .padding(.bottom, .screenWidth * 0.1)
+                HStack {
+                    Text("Email: ")
+                    TextField("Enter Email", text: $userEmail)
+                        .keyboardType(.emailAddress)
+                }
+                HStack {
+                    Text("Password: ")
+                    SecureField("Enter Password", text: $userPassword)
+                }
+                Button(action: {
+                    print("userEmail: \(userEmail)")
+                    print("userPassword: \(userPassword)")
+                    
+    //                Auth.auth().signIn(withEmail: userEmail, password: userPassword) { [weak self] authResult, error in
+    //                  guard let strongSelf = self else { return }
+    //                  // ...
+    //                }
+                }, label: {
+                    Text("Login")
+                })
             }
-            HStack {
-                Text("Password: ")
-                SecureField("Enter Password", text: $userPassword)
-            }
-            Button(action: {                
-                print("userEmail: \(userEmail)")
-                print("userPassword: \(userPassword)")
-                
-//                Auth.auth().signIn(withEmail: userEmail, password: userPassword) { [weak self] authResult, error in
-//                  guard let strongSelf = self else { return }
-//                  // ...
-//                }
-            }, label: {
-                Text("Login")
-            })
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding()
+            .navigationBarBackButtonHidden(true)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding()
-        .navigationBarBackButtonHidden(true)
-    }
+        }
+
 }
 
-#Preview {
-    LoginView()
+//#Preview {
+//    LoginView()
+//}
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
 }
